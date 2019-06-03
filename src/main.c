093c884e2f8e9e9da2fe4e8421998ffd72a05ae1
Akira Ar'Krein <Akira.ArKrein@gmail.com>
2019-06-04 01:17:05 +0700

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,45 @@
+#include "geometry.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char figure[N]; // c
+
+    FILE *coord;
+    coord = fopen("coord.txt", "r");
+
+    if (coord == NULL)
+    {
+        printf("File not found\n");
+        return -1;
+    }
+
+    Circle *cir = (Circle *)malloc(sizeof(Circle) * N);
+
+    int fl = 0;
+
+    while (!feof(coord))
+    {
+        fgets(figure, N, coord);
+        if (strcmp(figure, "\n\0") == 0)
+        {
+            continue;
+        }
+
+        fl++;
+        (cir + fl)->num = fl;
+
+        if (circle_coord(figure, cir, fl) == -1)
+        {
+            fl--;
+            continue;
+        }
+    }
+    print_circle(cir, fl);
+    fclose(coord);
+
+    return 0;
+}
